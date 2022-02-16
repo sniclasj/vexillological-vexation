150027811df9bc3eb5f46148d004dc44626deecf
sniclasj <sniclasj@gmail.com>
2022-02-16 13:41:32 +0000

    Add test for incorrect key in game

--- scripts/tests/vex-vex.test.js
+++ scripts/tests/vex-vex.test.js
@@ -16,4 +16,7 @@ describe("game object contains correct keys", () => {
     test("correct key exists", () => {
         expect("correct" in game).toBe(true);
     });
+    test("incorrect key exists", () => {
+        expect("incorrect" in game).toBe(true);
+    });
 });
