c250af1b3abd3f19eb2c2ba0eaa2b99e2562964f
sniclasj <sniclasj@gmail.com>
2022-02-23 18:42:01 +0000

    Add comments to start game function

--- scripts/vex-vex.js
+++ scripts/vex-vex.js
@@ -3,10 +3,10 @@ let game = {
     incorrect: 0,
     flag1: ["wales", "new-zealand"],
     flag2: ["wales", "new-zealand"],
-    country: ["wales","new-zealand"],
+    country: ["wales", "new-zealand"],
 };
 
-function newGame () {
+function newGame() {
     game.correct = 0;
     game.incorrect = 0;
     game.country = "???";
@@ -14,8 +14,16 @@ function newGame () {
     game.flag2 = "flag-id-0";
 };
 
-function startGame () {
+function startGame() {
+    // Plan is for this to generate a number which
+    // links to a country name in an object e.g. 
+    // let country1 = Math.floor(Math.random() * 25) + 1;
+    // along with var (countries) = {1:"Wales", 2:"New Zealand"}
     game.country = 1;
 };
 
-module.exports = { game, newGame, startGame };
+module.exports = {
+    game,
+    newGame,
+    startGame
+};
