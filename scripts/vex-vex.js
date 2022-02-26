8f34323d6ce054f2973c4c29d1fac05cfdb907f5
sniclasj <sniclasj@gmail.com>
2022-02-26 10:37:49 +0000

    Add code to prevent flag1 num being equal to flag2 num

--- scripts/vex-vex.js
+++ scripts/vex-vex.js
@@ -18,6 +18,18 @@ function startGame() {
     game.flag1 = Math.floor(Math.random() * 50) + 1;
     game.flag2 = Math.floor(Math.random() * 50) + 1;
     game.country = [game.flag1, game.flag2][Math.floor(Math.random() * 2)];
+    // Need to check the logic of the following if statement.
+    // Should check if flag1 = flag2, if yes, should generate
+    // a different flag2 number. Not sure if it is robust enough
+    // to guarantee that flag1 won't equal flag2 again!
+    if (game.flag1 = game.flag2) {
+        game.flag2 = Math.floor(Math.random() * 50) + 1;
+        game.country = [game.flag1, game.flag2][Math.floor(Math.random() * 2)];
+    } else {
+        game.flag1 = Math.floor(Math.random() * 50) + 1;
+        game.flag2 = Math.floor(Math.random() * 50) + 1;
+        game.country = [game.flag1, game.flag2][Math.floor(Math.random() * 2)];
+    };
 };
 
 module.exports = {
