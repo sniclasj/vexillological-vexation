44bbe9afd3796c7e589d38b2abbabb83beab411d
sniclasj <sniclasj@gmail.com>
2022-03-05 13:05:49 +0000

    Update flags key content

--- scripts/vex-vex.js
+++ scripts/vex-vex.js
@@ -1,6 +1,10 @@
 let game = {
-    flags: ["arg", "aut", "bel", "bol", "bra", "can", "cpv", "chl", "hrv", "cze", "dnk", "etr", "ecu", "egy", "est", "eth", "fra", "deu", "gha", "grl", "hnd", "isl", "irl", "imn", "ita", "civ", "jpn", "ken", "kir", "mdg", "mwi", "mlt", "nru", "nzl", "nga", "nir", "nor", "pan", "wsm", "sco", "sen", "syc", "zaf", "esp", "swe", "ukr", "ury", "usa", "vnm", "wal"],
-};
+    flags: {
+        flagNumber:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50],
+        flagIds: ["arg", "aut", "bel", "bol", "bra", "can", "cpv", "chl", "hrv", "cze", "dnk", "etr", "ecu", "egy", "est", "eth", "fra", "deu", "gha", "grl", "hnd", "isl", "irl", "imn", "ita", "civ", "jpn", "ken", "kir", "mdg", "mwi", "mlt", "nru", "nzl", "nga", "nir", "nor", "pan", "wsm", "sco", "sen", "syc", "zaf", "esp", "swe", "ukr", "ury", "usa", "vnm", "wal"],
+        countryIds:["Argentina","Austria","Bolivia","Brazil","Canada","Cape Verde","Chile","Croatia","Czech Republic","Denmark","East Timor","Ecuador","Egypt","Estonia","Ethiopia","France","Germany","Ghana","Greenland","Honduras","Iceland","Ireland","Isle of Man","Italy","Ivory Coast","Japan","Kenya","Kiribati","Madagascar","Malawi","Malta","Nauru","New Zealand","Nigeria","Northern Ireland","Norway","Panama","Samoa","Scotland","Senegal","Seychelles","South Africa","Spain","Sweden","Ukraine","Uruguay","USA","Vietnam","Wales"],
+    },
+    };
 
 module.exports = {
     game,
