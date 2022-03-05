7a00727926840609e5aae6df4b521a8ae84100d1
sniclasj <sniclasj@gmail.com>
2022-03-05 12:44:51 +0000

    Add flags game key

--- scripts/vex-vex.js
+++ scripts/vex-vex.js
@@ -1,5 +1,5 @@
 let game = {
-
+    flags: ["arg","aut","bel","bol","bra","can","cpv","chl","hrv","cze","dnk","etr","ecu","egy","est","eth","fra","deu","gha","grl","hnd","isl","irl","imn","ita","civ","jpn","ken","kir","mdg","mwi","mlt","nru","nzl","nga","nir","nor","pan","wsm","sco","sen","syc","zaf","esp","swe","ukr","ury","usa","vnm","wal"],
 };
 
 module.exports = {
