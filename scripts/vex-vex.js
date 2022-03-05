7a92f6eb08bcb63d7c1b5a04204891fb882d0663
sniclasj <sniclasj@gmail.com>
2022-03-05 13:33:26 +0000

    Add option1 key to game

--- scripts/vex-vex.js
+++ scripts/vex-vex.js
@@ -3,6 +3,7 @@ let game = {
         flags: ["arg", "aut", "bel", "bol", "bra", "can", "cpv", "chl", "hrv", "cze", "dnk", "etr", "ecu", "egy", "est", "eth", "fra", "deu", "gha", "grl", "hnd", "isl", "irl", "imn", "ita", "civ", "jpn", "ken", "kir", "mdg", "mwi", "mlt", "nru", "nzl", "nga", "nir", "nor", "pan", "wsm", "sco", "sen", "syc", "zaf", "esp", "swe", "ukr", "ury", "usa", "vnm", "wal"],
         countries:["Argentina","Austria","Belgium","Bolivia","Brazil","Canada","Cape Verde","Chile","Croatia","Czech Republic","Denmark","East Timor","Ecuador","Egypt","Estonia","Ethiopia","France","Germany","Ghana","Greenland","Honduras","Iceland","Ireland","Isle of Man","Italy","Ivory Coast","Japan","Kenya","Kiribati","Madagascar","Malawi","Malta","Nauru","New Zealand","Nigeria","Northern Ireland","Norway","Panama","Samoa","Scotland","Senegal","Seychelles","South Africa","Spain","Sweden","Ukraine","Uruguay","USA","Vietnam","Wales"],
     },
+    option1:[],
     };
 
 module.exports = {
