d6cc0148947b5ff0a8ccf828dc8b4df1de8fcbec
sniclasj <sniclasj@gmail.com>
2022-03-06 09:28:43 +0000

    Format js file document

--- scripts/vex-vex.js
+++ scripts/vex-vex.js
@@ -2,27 +2,28 @@
 let game = {
     database: {
         flags: ["arg", "aut", "bel", "bol", "bra", "can", "cpv", "chl", "hrv", "cze", "dnk", "etr", "ecu", "egy", "est", "eth", "fra", "deu", "gha", "grl", "hnd", "isl", "irl", "imn", "ita", "civ", "jpn", "ken", "kir", "mdg", "mwi", "mlt", "nru", "nzl", "nga", "nir", "nor", "pan", "wsm", "sco", "sen", "syc", "zaf", "esp", "swe", "ukr", "ury", "usa", "vnm", "wal"],
-        countries:["Argentina","Austria","Belgium","Bolivia","Brazil","Canada","Cape Verde","Chile","Croatia","Czech Republic","Denmark","East Timor","Ecuador","Egypt","Estonia","Ethiopia","France","Germany","Ghana","Greenland","Honduras","Iceland","Ireland","Isle of Man","Italy","Ivory Coast","Japan","Kenya","Kiribati","Madagascar","Malawi","Malta","Nauru","New Zealand","Nigeria","Northern Ireland","Norway","Panama","Samoa","Scotland","Senegal","Seychelles","South Africa","Spain","Sweden","Ukraine","Uruguay","USA","Vietnam","Wales"],
+        countries: ["Argentina", "Austria", "Belgium", "Bolivia", "Brazil", "Canada", "Cape Verde", "Chile", "Croatia", "Czech Republic", "Denmark", "East Timor", "Ecuador", "Egypt", "Estonia", "Ethiopia", "France", "Germany", "Ghana", "Greenland", "Honduras", "Iceland", "Ireland", "Isle of Man", "Italy", "Ivory Coast", "Japan", "Kenya", "Kiribati", "Madagascar", "Malawi", "Malta", "Nauru", "New Zealand", "Nigeria", "Northern Ireland", "Norway", "Panama", "Samoa", "Scotland", "Senegal", "Seychelles", "South Africa", "Spain", "Sweden", "Ukraine", "Uruguay", "USA", "Vietnam", "Wales"],
     },
-    option1:[],
-    option2:[],
-    };
+    option1: [],
+    option2: [],
+};
 
 // Function to randomly generate values for game.option1 and 
 // game.option2 from the game.database.flags array
-function optionGen () {
+function optionGen() {
     game.option1 = (game.database.flags[Math.floor(Math.random() * game.database.flags.length)]);
-//Filters game.option1 value out of game.database.flags array
-//and selects game.option2 value from filtered array. This
-//ensures that game.option1 != game.option2
-//Credit: https://www.w3schools.com/jsref/jsref_filter.asp
-//Credit: https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array
+    //Filters game.option1 value out of game.database.flags array
+    //and selects game.option2 value from filtered array. This
+    //ensures that game.option1 != game.option2
+    //Credit: https://www.w3schools.com/jsref/jsref_filter.asp
+    //Credit: https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array
     var value = game.option1;
     var arr = game.database.flags;
-    databaseFilt = arr.filter(function(item) {
-    return item !== value});
-//Generates game.option2 value from the filtered dabase which does
-//not contain the generated game.option1 value
+    databaseFilt = arr.filter(function (item) {
+        return item !== value
+    });
+    //Generates game.option2 value from the filtered dabase which does
+    //not contain the generated game.option1 value
     game.option2 = (databaseFilt[Math.floor(Math.random() * databaseFilt.length)]);
 };
 
