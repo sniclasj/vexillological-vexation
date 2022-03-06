80c42366abe7ee7cc58937f283a6984bf3b0df50
sniclasj <sniclasj@gmail.com>
2022-03-06 09:29:09 +0000

    Format test document

--- scripts/tests/vex-vex.test.js
+++ scripts/tests/vex-vex.test.js
@@ -19,10 +19,10 @@ describe("game object contains correct keys", () => {
     test("database key exists", () => {
         expect("database" in game).toBe(true);
     });
-        test("option1 key exists", () => {
+    test("option1 key exists", () => {
         expect("option1" in game).toBe(true);
     });
-        test("option2 key exists", () => {
+    test("option2 key exists", () => {
         expect("option2" in game).toBe(true);
     });
 });
@@ -31,20 +31,20 @@ describe("game object contains correct keys", () => {
 // game.option2 from the game.database.flags array
 describe("optionGen function works correctly", () => {
     beforeAll(() => {
-    game.option1 = [];
-    game.option2 = [];
-    optionGen();
-});
+        game.option1 = [];
+        game.option2 = [];
+        optionGen();
+    });
     test("option1 is generated with a random value from database key", () => {
         expect(game.database.flags).toContain(game.option1);
     });
-        test("option2 is generated with a random value from database key", () => {
+    test("option2 is generated with a random value from database key", () => {
         expect(game.database.flags).toContain(game.option2);
     });
     //Possible that option1 can be the same as option2
     //Test can provide false passes due to random value generation
     // Code in js file will ensure flase passes do not occur
-        test("random vaue for option1 != random value for option2", () => {
+    test("random vaue for option1 != random value for option2", () => {
         expect(game.option1).not.toBe(game.option2);
     });
 });
