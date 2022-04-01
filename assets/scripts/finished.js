b4b38637e3b74b14a8f31c756893d9107e4de0cf
sniclasj <sniclasj@gmail.com>
2022-04-01 14:30:06 +0000

    Correct syntax error in finished.js

--- assets/scripts/finished.js
+++ assets/scripts/finished.js
@@ -1,5 +1,5 @@
 /* jshint esversion: 8 */
-document.getElementById("game-result").innerHTML = `You scored X out of 20!`
+document.getElementById("game-result").innerHTML = `You scored X out of 20!`;
 document.getElementById("play-again-button").addEventListener('click', e => {
     location.href = 'vex-vex.html';
 });
