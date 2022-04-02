e1519606db7f6bfa1d2aec48a34d2c439ef34908
sniclasj <sniclasj@gmail.com>
2022-04-02 13:21:43 +0000

    Adocal storage code to finished.js

--- assets/scripts/finished.js
+++ assets/scripts/finished.js
@@ -1,5 +1,8 @@
 /* jshint esversion: 8 */
-document.getElementById("game-result").innerHTML = `You scored X out of 20!`;
+
+let score = localStorage.getItem("score");
+
+document.getElementById("game-result").innerHTML = `You scored `+(score)+` out of 20!`;
 document.getElementById("play-again-button").addEventListener('click', e => {
     location.href = 'vex-vex.html';
 });
