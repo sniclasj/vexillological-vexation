ff1c3e624e1d90ec4428c3ef068bdd8ae7e2246f
sniclasj <sniclasj@gmail.com>
2022-04-02 13:33:13 +0000

    Replace arrow function in event listener with regular function

--- assets/scripts/finished.js
+++ assets/scripts/finished.js
@@ -3,6 +3,6 @@
 let score = localStorage.getItem("score");
 
 document.getElementById("game-result").innerHTML = `You scored `+(score)+` out of 20!`;
-document.getElementById("play-again-button").addEventListener('click', e => {
+document.getElementById("play-again-button").addEventListener('click', function() {
     location.href = 'vex-vex.html';
 });
