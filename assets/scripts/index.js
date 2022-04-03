11242f06844866ac0fbed389f63bd2c38bd89262
sniclasj <sniclasj@gmail.com>
2022-04-03 09:32:50 +0000

    Update index.js document format

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,4 @@
 /* jshint esversion: 8 */
-document.getElementById("landing-page-start").addEventListener('click', function() {
+document.getElementById("landing-page-start").addEventListener('click', function () {
     location.href = 'vex-vex.html';
 });
