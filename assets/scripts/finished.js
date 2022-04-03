cf0f4c45fcf7e8a722c685dd35a193260895104e
sniclasj <sniclasj@gmail.com>
2022-04-03 09:04:48 +0000

    Add local storage retrieve code to finished.js

--- assets/scripts/finished.js
+++ assets/scripts/finished.js
@@ -1,8 +1,8 @@
 /* jshint esversion: 8 */
 
-let score = localStorage.getItem("score");
+let score = localStorage.getItem("result");
 
-document.getElementById("game-result").innerHTML = `You scored `+(score)+` out of 20!`;
+document.getElementById("game-result").innerHTML = `You scored ${score} out of 20!`;
 document.getElementById("play-again-button").addEventListener('click', function() {
     location.href = 'vex-vex.html';
 });
