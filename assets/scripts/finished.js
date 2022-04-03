fff74787bac2ea666688b5fbf0beda1a9d63a152
sniclasj <sniclasj@gmail.com>
2022-04-03 09:32:26 +0000

    Update finished.js document format

--- assets/scripts/finished.js
+++ assets/scripts/finished.js
@@ -1,12 +1,12 @@
 /* jshint esversion: 8 */
 
 let score = localStorage.getItem("result");
-let percent = ((score/20)*100);
+let percent = ((score / 20) * 100);
 
 document.getElementById("game-result").innerHTML = `You scored ${score} out of 20! That's ${percent}%`;
-document.getElementById("play-again-button").addEventListener('click', function() {
+document.getElementById("play-again-button").addEventListener('click', function () {
     location.href = 'vex-vex.html';
 });
-document.getElementById("back-to-start").addEventListener('click', function() {
+document.getElementById("back-to-start").addEventListener('click', function () {
     location.href = 'index.html';
 });
