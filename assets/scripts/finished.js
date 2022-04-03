5b29cced68a9ad4868aa4edc033819415becf487
sniclasj <sniclasj@gmail.com>
2022-04-03 09:18:17 +0000

    Add percent calc and add to template literal

--- assets/scripts/finished.js
+++ assets/scripts/finished.js
@@ -1,8 +1,9 @@
 /* jshint esversion: 8 */
 
 let score = localStorage.getItem("result");
+let percent = ((score/20)*100);
 
-document.getElementById("game-result").innerHTML = `You scored ${score} out of 20!`;
+document.getElementById("game-result").innerHTML = `You scored ${score} out of 20! That's ${percent}%`;
 document.getElementById("play-again-button").addEventListener('click', function() {
     location.href = 'vex-vex.html';
 });
