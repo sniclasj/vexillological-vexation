271dc05eaf8f912d5aff6f0063ed453a4513ec10
sniclasj <sniclasj@gmail.com>
2022-04-06 14:00:28 +0000

    Revert percent calculation to be done in one step

--- assets/scripts/finished.js
+++ assets/scripts/finished.js
@@ -2,7 +2,7 @@
 
 // Code credit: https://stackoverflow.com/questions/47817325/storing-my-game-score-in-local-storage Antoni
 let score = localStorage.getItem("result");
-let percent = ((score / 20) * 100);
+let percent = ((score / 20).toFixed(2))*100;
 
 document.getElementById("game-result").innerHTML = `You scored ${score} out of 20! That's ${percent}%`;
 document.getElementById("play-again-button").addEventListener('click', function () {
