d018fadb029b034d1e0495ded72de273b96b010c
sniclasj <sniclasj@gmail.com>
2022-04-18 10:52:14 +0000

    Add code comments to index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,6 @@
 /* jshint esversion: 8 */
+
+// Lets the user initiate a game via a click on the 'start' button
 document.getElementById("landing-page-start").addEventListener('click', function () {
     location.href = 'vex-vex.html';
 });
