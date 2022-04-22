2f015e2bc46b2aa680e51dc4cbc845213e68e68c
sniclasj <sniclasj@gmail.com>
2022-04-22 15:52:36 +0000

    Correct typo in percent calculation

--- assets/scripts/finished.js
+++ assets/scripts/finished.js
@@ -3,7 +3,7 @@
 // Calculate a percentage score from user's game result.
 // Code credit: https://stackoverflow.com/questions/47817325/storing-my-game-score-in-local-storage Antoni
 let score = localStorage.getItem("result");
-let percent = ((score / 20).toFixed(2)) * 100;
+let percent = (((score / 20)) * 100).toFixed(0);
 
 // Generates text in the game-result section in finished.html.
 document.getElementById("game-result").innerHTML = `You scored ${score} out of 20! That's ${percent}%`;
